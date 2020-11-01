86c774e3c27da16c2d3f070d3fba7551f476788a
Marcin <marcin-ficek@gazeta.pl>
2020-11-01 21:04:05 +0100

    Poprawienie pliku porownaniewartosci.c

--- porownaniewartosci.c
+++ porownaniewartosci.c
@@ -2,21 +2,9 @@
 #include<stdio.h>
 
 double minimalna(double a, double b){
-
-    if(a< b){
-        printf("\n%lf, jest mniejsze od %lf", a,b);
-        return a;
-    }
-    else if (a>b)
-    {
-        printf("\n%lf, jest mniejsze od %lf", b,a);
-        return b;
-    }
-    else if (a == b)
-    {
-        printf("\n%lf, jest rowne %lf",a,b);
-        return a;
-    }
+    if(a< b) return a; 
+    else if (a>b) return b;
+    else if (a == b) return a;
     else printf("\nNieprawidlowe dane");
 
     return 0.0;
@@ -25,5 +13,8 @@ double maksymalna(double x, double y){
     if (x>y) return x;
     else if (y>x) return y;
     else if (x==y) return x;
-    else if (x==0 && y==0) return 0;     
+    else if (x==0 && y==0) return 0;   
+    else printf("\nNieprawidlowe dane");
+
+    return 0.0;  
 }
